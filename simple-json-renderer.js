b79ce0903ce6ea2d36ce0685aa70d0802e49c869
Yifan Zhao <yifanzhao@hotmail.co.uk>
2015-02-28 01:45:32 +0000

    First version renderer

---
+++ simple-json-renderer.js
@@ -0,0 +1,45 @@
+var renderObject = (function() {
+function renderObj(obj) {
+  var elem = d3.select(this)
+  
+  if (_.isPlainObject(obj)) {
+    elem.classed('object', true)
+    elem.append('span').text('{')
+    var list = elem.append('ul')    
+    
+    _.forEach(obj, function(value, key) {
+      var item = list.append('li')
+      item.append('a')
+        .text(key + ' : ')
+        .on('click', toggleExpandedState)
+      
+      item.each(_.partial(renderObj, value))
+    })
+    elem.append('span').text('}')
+    
+  } else if (_.isArray(obj)) {
+    
+    elem.classed('array', true)
+    elem.append('span').text('[')
+    var list = elem.append('ul')
+    _.forEach(obj, function(value, key) {
+      list.append('li')
+        .each(_.partial(renderObj, value))
+    })
+    elem.append('span').text(']')    
+    
+  } else {
+    elem.classed('value', true)
+    elem.append('i')
+      .text(obj)
+  }
+}
+  
+  function toggleExpandedState() {
+    var listHeader = d3.select(this)
+    listHeader.classed('expanded', !listHeader.classed('expanded'))
+  }
+  return function(selector, obj) {
+    d3.select(selector).each(_.partial(renderObj, obj))
+  }
+})()
